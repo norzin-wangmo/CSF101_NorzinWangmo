0dd6e0ada69bb37357eb17b910c85b67e902a343
norzin-wangmo <norzin.wangmo@icloud.com>
2025-07-29 14:35:15 +0600

    Second commit

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,11 @@
 print("hello Norzin :) ")
 print ("Life is Exciting!")
+v = 2
+h = 3
+print (h+v)
+sum = h+v
+print ('so is the number', sum)
+print ('The sum of v + h is : ', sum)
+name = input ('Enter your name : ')
+age = input ('Enter your age : ')
+print ("You are ", name , ("and you are "), age, "years old.")
